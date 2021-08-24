6e9fb84cf86d1cb5432f10c8bc2effc00d1759ad
iqranoor <iniqra11@gmail.com>
2021-08-25 00:12:59 +0530

    Initial commit

--- bin/custom/NagAcc/NagAccfulfilmentprocess/src/de/hybris/NagAcc/fulfilmentprocess/actions/order/CheckDesignerAction.java
+++ bin/custom/NagAcc/NagAccfulfilmentprocess/src/de/hybris/NagAcc/fulfilmentprocess/actions/order/CheckDesignerAction.java
@@ -3,35 +3,32 @@ package de.hybris.NagAcc.fulfilmentprocess.actions.order;
 import de.hybris.NagAcc.core.model.DesignerModel;
 import de.hybris.platform.commerceservices.customer.CustomerService;
 import de.hybris.platform.commerceservices.enums.CustomerType;
+import de.hybris.platform.commerceservices.strategies.CheckoutCustomerStrategy;
 import de.hybris.platform.core.PasswordsReEncoder;
 import de.hybris.platform.core.model.order.OrderModel;
 import de.hybris.platform.core.model.user.CustomerModel;
 import de.hybris.platform.core.model.user.UserModel;
+import de.hybris.platform.order.OrderService;
 import de.hybris.platform.orderprocessing.model.OrderProcessModel;
 import de.hybris.platform.processengine.action.AbstractSimpleDecisionAction;
 import de.hybris.platform.servicelayer.user.UserService;
 import de.hybris.platform.task.RetryLaterException;
+import org.apache.log4j.Logger;
+
+import java.util.Locale;
 
 public class CheckDesignerAction extends AbstractSimpleDecisionAction<OrderProcessModel> {
 
-    private UserService userService;
-
-    public UserService getUserService() {
-        return userService;
-    }
-
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
-
+    private static final String ANONYMOUS_USER = "anonymous";
     @Override
     public Transition executeAction(OrderProcessModel orderProcessModel) throws RetryLaterException, Exception {
-        UserModel userModel = userService.getCurrentUser();
+        UserModel userModel = orderProcessModel.getOrder().getUser();
+        String userUid = userModel.getUid();
         if (userModel instanceof CustomerModel)
         {
-            CustomerModel customerModel = (CustomerModel) userModel;
-            boolean isRegisteredUser = !(getUserService().isAnonymousUser(getUserService().getCurrentUser()));
-            if(isRegisteredUser){
+            CustomerModel customerModel = (CustomerModel) orderProcessModel.getOrder().getUser();
+            boolean isAnonymous = userUid.equals(ANONYMOUS_USER);
+            if(!isAnonymous){
                 final OrderModel orderModel = orderProcessModel.getOrder();
                 DesignerModel orderDesigner = orderModel.getDesigner();
                 if (orderDesigner != null) // if designer is set i.e when all products in the order have same designer
@@ -43,8 +40,9 @@ public class CheckDesignerAction extends AbstractSimpleDecisionAction<OrderProce
                     }
                 }
             }
+           return Transition.OK;
         }
-        return Transition.OK;
+        return Transition.NOK;
 
     }
 }
